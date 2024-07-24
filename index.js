7f5825d0cc6d034bcd3fda9bc9a3577ab73e3322
Javidanhaj <hacizadecavidan.ch@gmail.com>
2024-07-24 11:43:33 +0400

    .

---
+++ index.js
@@ -0,0 +1,15 @@
+function clock_logic(){
+    const now = new Date();
+
+    let hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    
+    const clock = `${hours}:${minutes}:${seconds}`; 
+
+    document.getElementById('clock').innerText = clock;
+
+}
+
+clock_logic();
+setInterval(clock_logic, 1000);
